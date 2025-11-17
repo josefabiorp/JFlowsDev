f08aa5b479feee9d28cd6e44e7e977789ac95e1f
josefabiorp <71747818+josefabiorp@users.noreply.github.com>
2025-11-17 15:53:09 -0600

    Refactor LoadingPage to use imported assets

--- src/LoadingPage.jsx
+++ src/LoadingPage.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
+
 import { ImgLoadingPage } from './components/activity/ImageLoadingPage.jsx';
 import { CardLoadingPage } from './components/activity/CardLoading.jsx';
 import { EndSectionLoad } from './components/activity/EndSectionLoad.jsx';
 import { Header } from '../src/components/Header.jsx';
 import { Footer } from '../src/components/Footer.jsx';
 import { PriceCard } from './components/activity/PriceCard.jsx';
-export function  LoadingPage() {
 
-  return ( 
+// 🔥 Importación correcta de imágenes (compatible con Vite/Vercel)
+import loadingpage from "../src/assets/loadingpage.png";
+import fast from "../src/assets/fast.svg";
+import efficient from "../src/assets/efficient.svg";
+import calendar from "../src/assets/calendar.svg";
+import users from "../src/assets/users.svg";
+import bg1 from "../src/assets/bg1.jpg";
+
+export function LoadingPage() {
+  return (
     <div>
-      <Header/>
-      <ImgLoadingPage image=".\src\assets\loadingpage.png"/>
-      <CardLoadingPage image00=".\src\assets\fast.svg" image01=".\src\assets\efficient.svg" image02=".\src\assets\calendar.svg" image03=".\src\assets\users.svg"/>
-      <EndSectionLoad image=".\src\assets\bg1.jpg"/>
-      <PriceCard></PriceCard>
-      <Footer/>
+      <Header />
+
+      <ImgLoadingPage image={loadingpage} />
+
+      <CardLoadingPage
+        image00={fast}
+        image01={efficient}
+        image02={calendar}
+        image03={users}
+      />
+
+      <EndSectionLoad image={bg1} />
+
+      <PriceCard />
+
+      <Footer />
     </div>
-      )
+  );
 }
