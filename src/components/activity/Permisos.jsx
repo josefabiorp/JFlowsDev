25ff26b571a2a7a3a35719c76192fecd34ddc551
josefabiorp <71747818+josefabiorp@users.noreply.github.com>
2025-11-17 20:38:57 -0600

    Update Permisos.jsx

--- src/components/activity/Permisos.jsx
+++ src/components/activity/Permisos.jsx
@@ -7,7 +7,7 @@ import { useAccountManagement } from "../hooks/useAccountManagement.js";
 import { usePermisos } from "../hooks/usePermisos.js";
 import { AppModal } from "./AppModal.jsx";
 
-const API_URL = "https://jflowsdev.duckdns.org";
+const API_URL = "https://jflowsdev.duckdns.org/api";
 
 export function Permisos() {
   const { token, user, isAdmin } = useUser();
